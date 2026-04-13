d4a98d1ce64b7943174466fd0c8b9d31923786e8
gitmorgen <110793507+gitmorgen@users.noreply.github.com>
2026-04-13 12:50:17 +0300

    style fix

--- src/components/organisms/ToastContainer.svelte
+++ src/components/organisms/ToastContainer.svelte
@@ -8,7 +8,8 @@
 </script>
 
 <!-- Toast Container -->
-<div class="fixed top-4 left-1/2 z-60 flex -translate-x-1/2 flex-col items-center gap-2">
+<!-- <div class="fixed top-4 left-1/2 z-60 flex -translate-x-1/2 flex-col items-center gap-2"> -->
+<div class="fixed top-4 z-60 flex justify-center flex-col items-center gap-2 px-4 w-full">
 	{#each $toasts as toast (toast.id)}
 		<Toast
 			message={toast.message}
